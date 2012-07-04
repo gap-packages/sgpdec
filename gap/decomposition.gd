3d80500baee4c76471e9bc31da77320b102a2266
Attila Egri-Nagy <attila@egri-nagy.hu>
2012-07-04 12:30:11 +0200

    order wrong in decomposition.gd

--- gap/decomposition.gd
+++ gap/decomposition.gd
@@ -9,6 +9,12 @@
 ## The abstract datatype for hierarchical decompositions.
 ##
 
+DeclareCategory("IsHierarchicalDecomposition", IsDenseList);
+DeclareRepresentation( "IsHierarchicalDecompositionRep",
+                       IsComponentObjectRep,
+                       [ "original",
+                         "cascadedstruct"] );
+
 DeclareGlobalFunction("OriginalStructureOf");
 DeclareOperation("CascadedStructureOf",[IsObject]);
 DeclareOperation("Raise",[IsHierarchicalDecomposition,IsObject]);
@@ -18,9 +24,3 @@ DeclareOperation("ComponentActions",
         [IsHierarchicalDecomposition,IsObject,IsObject]);
 DeclareOperation("x2y",[IsHierarchicalDecomposition,IsObject,IsObject]);
 DeclareGlobalFunction("DependencyClassesOfDecomposition");
-
-DeclareCategory("IsHierarchicalDecomposition", IsDenseList);
-DeclareRepresentation( "IsHierarchicalDecompositionRep",
-                       IsComponentObjectRep,
-                       [ "original",
-                         "cascadedstruct"] );
