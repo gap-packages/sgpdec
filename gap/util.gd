8fd24ac4f886ddef726f48b4086484a31922cd64
Attila Egri-Nagy <attila@egri-nagy.hu>
2012-07-21 15:55:59 +1000

    util functions to get proper .gi .gd files

---
+++ gap/util.gd
@@ -0,0 +1,11 @@
+#############################################################################
+##
+##  util.gi  SgpDec package
+##
+##  (C)  2011-2012 Attila Egri-Nagy, Chrystopher L. Nehaniv, James D. Mitchell
+##
+##  Some utility methods.
+##
+
+DeclareGlobalFunction("FormattedMemoryString");
+DeclareGlobalFunction("FormattedTimeString");
