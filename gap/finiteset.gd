ba4660b4519caa4b32418508f6d463f7a8df6715
Attila Egri-Nagy <attila@egri-nagy.hu>
2012-07-28 11:55:39 +1000

    revising declarations, copy constructor becomes global function

--- gap/finiteset.gd
+++ gap/finiteset.gd
@@ -2,54 +2,55 @@
 ##
 ## finiteset.gd           SgpDec package
 ##
-## Copyright (C)  Attila Egri-Nagy, Chrystopher L. Nehaniv
+## Copyright (C) 2008-2012
 ##
-## 2008-2009 University of Hertfordshire, Hatfield, UK
+## Attila Egri-Nagy, Chrystopher L. Nehaniv, James D. Mitchell
 ##
-## Finite sets of integers.
+## Finite sets of integers. Wrapping boolean lists for speed and readability
+## purposes
 ##
-
-DeclareCategory( "IsFiniteSet", IsListOrCollection );
-
-
-DeclareRepresentation( "IsFiniteSetBlistRep",
-                       IsComponentObjectRep ,
-                       [ "blist" ] );
-
-#GLOBAL VARIABLES FOR TYPE INFO
-_FiniteSetsFamily :=  NewFamily( "FiniteSetsFamily", IsFiniteSet,
-                       CanEasilySortElements, CanEasilySortElements );
-_FiniteSetType := NewType( _FiniteSetsFamily,
-                          IsFiniteSet and IsFiniteSetBlistRep );
-_FiniteSetDomain := Domain(_FiniteSetsFamily,[]);
 
 #constructors
 DeclareGlobalFunction("FiniteSet");
 DeclareGlobalFunction("FiniteSetByBlist");
+DeclareGlobalFunction("FiniteSetCopy");
 #related to size
-DeclareGlobalFunction( "SizeOfUniverse" );
-DeclareGlobalFunction( "IsSingleton" );
+DeclareGlobalFunction("SizeOfUniverse");
+DeclareGlobalFunction("IsSingleton");
 #action on finite sets
-DeclareGlobalFunction( "OnFiniteSets" );
-DeclareGlobalFunction( "Transf2PermOnSet" );
-DeclareGlobalFunction( "IsIdentityOnFiniteSet" );
-
-DeclareGlobalFunction( "IsProperFiniteSubset" );
-DeclareGlobalFunction( "UnionFiniteSet" );
-DeclareGlobalFunction( "IntersectionFiniteSet" );
-DeclareGlobalFunction( "DifferenceFiniteSet" );
-DeclareGlobalFunction( "ComplementOfFiniteSet" );
-
+DeclareGlobalFunction("OnFiniteSets");
+DeclareGlobalFunction("Transf2PermOnSet");
+DeclareGlobalFunction("IsIdentityOnFiniteSet");
+#set operations that create a new set
+DeclareGlobalFunction("IsProperFiniteSubset");
+DeclareGlobalFunction("UnionFiniteSet");
+DeclareGlobalFunction("IntersectionFiniteSet");
+DeclareGlobalFunction("DifferenceFiniteSet");
+DeclareGlobalFunction("ComplementOfFiniteSet");
+#set operations that changes set
 DeclareGlobalFunction("UniteFiniteSet");
 DeclareGlobalFunction("UniteFiniteSets");
-DeclareGlobalFunction( "IntersectFiniteSet" );
-DeclareGlobalFunction( "SubtractFiniteSet" );
-DeclareGlobalFunction( "ComplementFiniteSet" );
+DeclareGlobalFunction("IntersectFiniteSet");
+DeclareGlobalFunction("SubtractFiniteSet");
+DeclareGlobalFunction("ComplementFiniteSet");
 
-DeclareGlobalFunction( "AllSubsets" );
+DeclareGlobalFunction("AllSubsets");
 
 DeclareGlobalFunction("FiniteSetPrinter");
 DeclareGlobalFunction("FiniteSetID");
 
 DeclareGlobalFunction("MinFiniteSet");
 DeclareGlobalFunction("MaxFiniteSet");
+
+DeclareCategory("IsFiniteSet", IsListOrCollection);
+
+DeclareRepresentation("IsFiniteSetBlistRep",
+                       IsComponentObjectRep ,
+                       [ "blist" ]);
+
+#GLOBAL VARIABLES FOR TYPE INFO
+_FiniteSetsFamily :=  NewFamily("FiniteSetsFamily", IsFiniteSet,
+                       CanEasilySortElements, CanEasilySortElements);
+_FiniteSetType := NewType( _FiniteSetsFamily,
+                          IsFiniteSet and IsFiniteSetBlistRep);
+_FiniteSetDomain := Domain(_FiniteSetsFamily,[]);
