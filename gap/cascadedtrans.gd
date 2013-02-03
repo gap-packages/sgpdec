a7ddf67cbe0f46a0d53550014d3a90691768b46d
James Mitchell <jenkyfunds@gmail.com>
2013-02-03 12:32:04 +0000

    more rearranging. JDm

--- gap/cascadedtrans.gd
+++ gap/cascadedtrans.gd
@@ -9,22 +9,24 @@
 ## Cascaded permutations and transformations.
 ##
 
-DeclareCategory("IsCascadedTransformation", IsMultiplicativeElementWithOne and IsAssociativeElement);
-DeclareCategoryCollections("IsCascadedTransformation");
+DeclareCategory("IsCascadedTransformation", IsMultiplicativeElementWithOne and
+IsAssociativeElement and IsAttributeStoringRep);
 
-BindGlobal("CascadedTransformationFamily", NewFamily("CascadedTransformationFamily",
+BindGlobal("CascadedTransformationFamily",
+NewFamily("CascadedTransformationFamily",
  IsCascadedTransformation, CanEasilySortElements, CanEasilySortElements));
 
 BindGlobal("CascadedTransformationType", NewType(CascadedTransformationFamily,
  IsCascadedTransformation and IsAssociativeElement));
 
-DeclareOperation("DomainsOfComponentsOfCascadeProduct", [IsList]);
+DeclareGlobalFunction("CascadedTransformationNC");
+DeclareGlobalFunction("CascadedTransformation");
 
+#old
 
 DeclareOperation("CascadeShellOf",[IsObject]);
 
 DeclareGlobalFunction("IdentityCascadedTransformation");
-DeclareGlobalFunction("CascadedTransformation");
 DeclareGlobalFunction("OnCoordinates");
 DeclareGlobalFunction("ImageListOfActionOnCoords");
 DeclareGlobalFunction("RandomCascadedTransformation");
@@ -36,19 +38,6 @@ DeclareGlobalFunction("IsDependencyCompatible");
 DeclareGlobalFunction("IsDependencyCompatibleOnPrefix");
 DeclareGlobalFunction("MonomialWreathProductGenerators");
 DeclareGlobalFunction("DotCascadedTransformation");
-#TODO IsMultiplicativeElement - IS there sg more spec shared by trans&perms?
-DeclareOperation("AsCascadedTrans",[IsMultiplicativeElement,IsCascadeShell]);
-DeclareOperation("AsCascadedTransNC",[IsMultiplicativeElement,IsCascadeShell]);
+#DeclareOperation("AsCascadedTrans",[IsMultiplicativeElement,IsCascadeShell]);
+#DeclareOperation("AsCascadedTransNC",[IsMultiplicativeElement,IsCascadeShell]);
 
-#we need the 'WithInverse' property to build groups of cascaded
-#transformation though not all of them have inverses
-
-CascadedTransformationFamily := NewFamily("CascadedTransformationFamily",
-                                   IsCascadedTransformation);
-CascadedTransformationRepresentation :=
-    NewRepresentation("CascadedTransformationRepresentation",
-    IsComponentObjectRep,["depfunc","csh"]);
-
-CascadedTransformationType := NewType(CascadedTransformationFamily,
-                                 IsCascadedTransformation and
-                                 CascadedTransformationRepresentation);
