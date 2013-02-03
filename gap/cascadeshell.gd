f5b725227959ecee0bd48df46988f1e50f4cb82d
Attila Egri-Nagy <attila@egri-nagy.hu>
2013-02-03 15:53:03 +0000

    removing CascadeShell code

--- gap/cascadeshell.gd
+++ gap/cascadeshell.gd
@@ -21,7 +21,6 @@ DeclareAttribute("DomainsOfCascadeProductComponents", IsCascadeProduct);
 DeclareAttribute("CascadeProductComponents", IsCascadeProduct);
 DeclareAttribute("DomainOfCascadeProduct", IsCascadeProduct);
 
-DeclareOperation("CascadeShell",[IsList,IsList,IsList]);
 DeclareGlobalFunction("AllCoords");
 DeclareGlobalFunction("CoordValSets");
 DeclareGlobalFunction("NameOfDependencyDomain");
@@ -29,21 +28,3 @@ DeclareGlobalFunction("CoordValConverter");
 DeclareGlobalFunction("CoordTransConverter");
 DeclareGlobalFunction("SizeOfWreathProduct");
 DeclareGlobalFunction("NumberOfDependencyFunctionArguments");
-
-DeclareCategory("IsCascadeShell", IsDenseList);
-DeclareProperty("IsCascadedGroupShell", IsCascadeShell);
-DeclareRepresentation(
-        "IsCascadeShellRep",
-        IsComponentObjectRep,
-        [ "components", #the building blocks
-          "name_of_shell", #a short name constructed from the components
-          "coordval_converters", #functions for convert coord vals (e.g. names)
-          "coordtrans_converters", #functions for converting coord trans
-          "depdom_names", #the names of dependency domains
-          "coordval_sets", #coordinate values (the original statesets of comps)
-          "allcoords", #all coordinate tuples (lazy storage)
-          "num_of_dependency_entries" #number of distinct dep function arguments
-          ]);
-CascadeShellType :=
-  NewType(NewFamily("CascadeShellFamily",IsCascadeShell),
-          IsCascadeShell and IsCascadeShellRep);
