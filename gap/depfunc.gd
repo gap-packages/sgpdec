18cd6781f1c749322e9ec8c24a0de75edb4d89a3
Attila Egri-Nagy <attila@egri-nagy.hu>
2013-03-05 13:59:58 +1100

    separating dependency function stuff from cascade

---
+++ gap/depfunc.gd
@@ -0,0 +1,14 @@
+#############################################################################
+##
+## depfunc.gd           SgpDec package
+##
+## (C)  Attila Egri-Nagy, Chrystopher L. Nehaniv, James D. Mitchell
+##
+## 2008-2012
+##
+## Dependency functions.
+##
+
+DeclareCategory("IsDependencyFunc", IsRecord);
+DeclareGlobalFunction("CreateDependencyDomains");
+DeclareGlobalFunction("CreateDependencyFunction");
