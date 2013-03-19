6ceb18d1f5fecb555a9be20e3aa432de7e60ddea
Attila Egri-Nagy <attila@egri-nagy.hu>
2013-03-19 22:32:14 +1100

    just evening commit, some things are broken

--- gap/cascadeprod.gd
+++ gap/cascadeprod.gd
@@ -14,6 +14,7 @@ DeclareCategoryCollections("IsCascade");
 DeclareSynonymAttr("IsCascadeSemigroup", IsSemigroup and
         IsCascadeCollection);
 
+DeclareGlobalFunction("CascadeGroup");
 DeclareCategoryCollections("IsPermCascade");
 DeclareSynonymAttr("IsCascadeGroup", IsGroup and
         IsPermCascadeCollection);
