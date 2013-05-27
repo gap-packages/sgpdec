bdfdc0dacb5aee2461499e6248d94e7e1489ec2a
James Mitchell <jenkyfunds@gmail.com>
2013-05-27 18:09:02 +0100

    moved everything out of cascadeproduct.g* to cascadesemigroup.g*

--- gap/cascadesemigroup.gd
+++ gap/cascadesemigroup.gd
@@ -12,8 +12,22 @@
 DeclareGlobalFunction("CascadeSemigroup");
 DeclareSynonym("IsCascadeSemigroup", IsSemigroup and IsCascadeCollection);
 
+DeclareProperty("IsFullCascadeProduct", IsCascadeSemigroup);
+
+DeclareAttribute("ComponentsOfCascadeProduct", IsCascadeSemigroup);
+DeclareAttribute("DomainOf", IsCascadeSemigroup);
+
+DeclareProperty("IsListOfPermGroupsAndTransformationSemigroups",
+        IsListOrCollection);
+DeclareAttribute("ComponentDomains", IsCascadeSemigroup);
+
+DeclareGlobalFunction("SizeOfFullCascadeProduct");
+DeclareGlobalFunction("SizeOfIteratedTransformationWreathProduct",
+        SizeOfFullCascadeProduct);
 DeclareProperty("IsFullCascadeSemigroup",
         IsCascadeSemigroup and IsFullCascadeProduct);
 DeclareSynonym("IsSemigroupWreathProduct", IsFullCascadeSemigroup);
 DeclareGlobalFunction("FullCascadeSemigroup");
 DeclareSynonym("SemigroupWreathProduct", FullCascadeSemigroup);
+
+DeclareSynonym("IsCascadeProduct", IsCascadeSemigroup);
