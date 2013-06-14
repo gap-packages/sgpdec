ae38d16af7588260104d5b12f69b5261201b3517
Attila Egri-Nagy <attila@egri-nagy.hu>
2013-06-14 21:08:27 +1000

    3rd gen skeleton started

---
+++ gap/sk.gd
@@ -0,0 +1,9 @@
+DeclareCategory("IsSKELETON", IsObject and IsAttributeStoringRep);
+BindGlobal("SKELETONFamily",NewFamily("SKELETONFamily", IsSKELETON));
+BindGlobal("SKELETONType", NewType(SKELETONFamily,IsSKELETON));
+
+DeclareGlobalFunction("SKELETON");
+DeclareAttribute("TransSgp",IsSKELETON);
+DeclareAttribute("BaseSet",IsSKELETON);
+DeclareAttribute("Generators",IsSKELETON);
+DeclareAttribute("DegreeOfSKELETON",IsSKELETON);
